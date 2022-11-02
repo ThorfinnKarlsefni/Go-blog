9b5e7880c4b714c040014b021d030aca06d81905
Thorfinn <402832626@qq.com>
2022-11-02 20:18:42 +0800

    test http.ServeMux

--- main.go
+++ main.go
@@ -3,23 +3,47 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html;charset=utf-8")
 	if r.URL.Path == "/" {
 		fmt.Fprintf(w, "<h1>Hello ,Cheung!<h1>")
-	} else if r.URL.Path == "/about" {
-		fmt.Fprintf(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
-			"<a href=\"mailto:402832626@qq.com\">Nick@qq.com")
 	} else {
 		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "<h1>请求页面未找到:(</h1)"+"<p>如有疑惑,请联系我们。</p>")
+		fmt.Fprint(w, "<h1>请求页面未找到:(</h1></br>"+
+			"<p>如有疑惑,请联系我们.</p>")
 	}
 }
 
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html;chartext=utf-8")
+	fmt.Fprintf(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
+		"<a href=\"mailto:402832626@qq.com\">Nick@qq.com </a>")
+}
+
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+	router := http.NewServeMux()
+
+	router.HandleFunc("/", defaultHandler)
+	router.HandleFunc("/about", aboutHandler)
+
+	// article details
+	router.HandleFunc("/articles/", func(w http.ResponseWriter, r *http.Request) {
+		id := strings.SplitN(r.URL.Path, "/", 3)[0]
+		fmt.Fprint(w, "文章 ID:"+id)
+	})
+
+	// lists or created
+	router.HandleFunc("/articles", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case "GET":
+			fmt.Fprint(w, "访问文章列表")
+		case "POST":
+			fmt.Fprint(w, "创建新的文章")
+		}
+	})
+	http.ListenAndServe(":3000", router)
 
 }
