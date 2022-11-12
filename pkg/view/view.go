1164cf9fd16afa7eeb3c7c2772fc6cab671c9d11
Thorfinn <402832626@qq.com>
2022-11-12 21:39:19 +0800

    register page

--- pkg/view/view.go
+++ pkg/view/view.go
@@ -3,30 +3,41 @@ package view
 import (
 	"goblog/pkg/logger"
 	"goblog/pkg/route"
+	"html/template"
 	"io"
 	"path/filepath"
 	"strings"
-	"text/template"
 )
 
+type D map[string]interface{}
+
 func Render(w io.Writer, data interface{}, tplFiels ...string) {
+	RenderTemplate(w, "app", data, tplFiels...)
+}
+
+func RenderSimple(w io.Writer, data interface{}, tplFiles ...string) {
+	RenderTemplate(w, "simple", data, tplFiles...)
+}
+
+func RenderTemplate(w io.Writer, name string, data interface{}, tplFiles ...string) {
 	viewDir := "resources/views/"
 
-	for i, f := range tplFiels {
-		tplFiels[i] = viewDir + strings.Replace(f, ".", "/", -1) + ".gohtml"
+	for i, f := range tplFiles {
+		tplFiles[i] = viewDir + strings.Replace(f, ".", "/", -1) + ".gohtml"
 	}
 
 	layoutFiles, err := filepath.Glob(viewDir + "layouts/*.gohtml")
+	logger.LogError(err)
 
-	allFiles := append(layoutFiles, tplFiels...)
+	allFiles := append(layoutFiles, tplFiles...)
 
 	tmpl, err := template.New("").
 		Funcs(template.FuncMap{
 			"RouteName2URL": route.Name2URL,
 		}).ParseFiles(allFiles...)
-
 	logger.LogError(err)
 
-	err = tmpl.ExecuteTemplate(w, "app", data)
+	err = tmpl.ExecuteTemplate(w, name, data)
+
 	logger.LogError(err)
 }
