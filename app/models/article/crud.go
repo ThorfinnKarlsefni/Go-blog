b312ea2edd04eb82476d25ca7943e5156d3dda74
Thorfinn <402832626@qq.com>
2022-11-15 22:59:28 +0800

    simple paging

--- app/models/article/crud.go
+++ app/models/article/crud.go
@@ -1,10 +1,12 @@
 package article
 
 import (
-	"fmt"
 	"goblog/pkg/logger"
 	"goblog/pkg/model"
+	"goblog/pkg/pagination"
+	"goblog/pkg/route"
 	"goblog/pkg/types"
+	"net/http"
 )
 
 func Get(idstr string) (Article, error) {
@@ -19,15 +21,21 @@ func Get(idstr string) (Article, error) {
 	return article, nil
 }
 
-func GetAll() ([]Article, error) {
+func GetAll(r *http.Request, perPage int) ([]Article, pagination.ViewData, error) {
+
+	// 1. 初始化分页实例
+	db := model.DB.Model(Article{}).Order("created_at desc")
+	_pager := pagination.New(r, db, route.Name2URL("home"), perPage)
+
+	// 2. 获取视图数据
+	viewData := _pager.Paging()
+
+	// 3. 获取数据
 	var articles []Article
 
-	if err := model.DB.Preload("User").Find(&articles).Error; err != nil {
-		fmt.Println(articles)
-		return articles, err
-	}
+	_pager.Results(&articles)
 
-	return articles, nil
+	return articles, viewData, nil
 }
 
 func (article *Article) Create() (err error) {
